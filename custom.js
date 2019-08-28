588e13ea033067ed1513b6f430eeb0cd016ca5f5
daren_race <daren.race@oracle.com>
2019-08-28 08:41:01 -0700

    updated js

--- custom.js
+++ custom.js
@@ -1,13 +1,21 @@
 if($(IPython.toolbar.selector.concat(' > #kill-run-all')).length == 0){
   IPython.toolbar.add_buttons_group([
         {
-             'label'   : 'kill and run-all',
-             'icon'    : 'fa fa-angle-double-down',
+             'label'   : 'kill-run-all',
+             'icon'    : 'fa fa-window-close',
              'callback': function(){
                  IPython.notebook.kernel.restart();
                  $(IPython.events).one('kernel_ready.Kernel',
-                                       function(){IPython.notebook.execute_all_cells();});
+                    function(){IPython.notebook.execute_all_cells();});
              }
         }
     ], 'kill-run-all');
 }
+if($(IPython.toolbar.selector.concat(' > #run-all')).length == 0){
+  IPython.toolbar.add_buttons_group([
+        {
+             'label'   : 'run-all',
+             'icon'    : 'fa fa-chevron-circle-right',
+             'callback': function(){
+                IPython.notebook.execute_all_cells();
+             }
